b25d92e68079e1743a1811033d1827034b5d1d0e
Sidsector9 <siddharth.thevaril@gmail.com>
2017-07-25 20:07:03 +0530

    Add more commands

--- ee-loader.php
+++ ee-loader.php
@@ -15,48 +15,49 @@ class EE_Command extends WP_CLI_Command {
 
 	public $printer_array      = array();
 	public $site_data          = array(
-									'html' => array(
-										'site-type' => 'html',
-									),
-									'mysql' => array(
-										'site-type' => 'php',
-										'mysql' => 'yes',
-										'php' => '5.6',
-									),
-									'php' => array(
-										'site-type' => 'php',
-										'php' => '5.6',
-									),
-									'php7' => array(
-										'php' => '7.0',
-									),
-									'wp' => array(
-										'site-type' => 'WordPress',
-										'mysql' => 'yes',
-										'php' => '5.6',
-									),
-									'wpfc' => array(
-										'site-type' => 'WordPress',
-										'mysql' => 'yes',
-										'php' => '5.6',
-										'cache-type' => 'nginx fastcgi_cache',
-									),
-									'wpredis' => array(
-										'site-type' => 'WordPress',
-										'mysql' => 'yes',
-										'php' => '5.6',
-										'cache-type' => 'nginx redis_cache',
-									),
-									'letsencrypt' => array(
-										'letsencrypt' => 'enabled',
-									),
-								);
+		'html' => array(
+			'site-type'  => 'html',
+		),
+		'mysql' => array(
+			'site-type'  => 'php',
+			'mysql'      => 'yes',
+			'php'        => '5.6',
+		),
+		'php' => array(
+			'site-type'  => 'php',
+			'php'        => '5.6',
+		),
+		'php7' => array(
+			'php'        => '7.0',
+		),
+		'wp' => array(
+			'site-type'  => 'WordPress',
+			'mysql'      => 'yes',
+			'php'        => '5.6',
+		),
+		'wpfc' => array(
+			'site-type'   => 'WordPress',
+			'mysql'       => 'yes',
+			'php'         => '5.6',
+			'cache-type'  => 'nginx fastcgi_cache',
+		),
+		'wpredis' => array(
+			'site-type'   => 'WordPress',
+			'mysql'       => 'yes',
+			'php'         => '5.6',
+			'cache-type'  => 'nginx redis_cache',
+		),
+		'letsencrypt' => array(
+			'letsencrypt' => 'enabled',
+		),
+	);
 
 	/**
 	 * @when before_wp_load
 	 */
 	public function create( $_, $assoc_args ) {
 		if ( empty( $_[0] ) ) {
+			WP_CLI::error( 'Site name is missing!' );
 			return;
 		} else {
 			$this->site_name = $_[0];
@@ -119,14 +120,16 @@ class EE_Command extends WP_CLI_Command {
 	 * @when before_wp_load
 	 */
 	public function info( $_, $assoc_args ) {
+		if ( empty( $_[0] ) ) {
+			WP_CLI::error( 'Site name is empty!' );
+			return;
+		}
+
 		if ( false === $this->_connect_to_db( 'localhost', 'root', 'AqRcKrLo' ) ) {
 			return;
 		}
 
-		$query = "
-					SELECT * FROM ee.ee_site_data
-					WHERE ee_site_data.site_name = '" . $_[0] . "'
-				";
+		$query = "SELECT * FROM ee.ee_site_data WHERE ee_site_data.site_name = '" . $_[0] . "'";
 
 
 		if ( $result = $this->conn->query( $query ) ) {
@@ -202,9 +205,42 @@ class EE_Command extends WP_CLI_Command {
 	 * @when before_wp_load
 	 */
 	public function delete( $_, $assoc_args ) {
-		if ( WP_CLI\Utils\get_flag_value( $assoc_args, 'no-prompt' ) ) {
-			
+		if ( empty( $_[0] ) ) {
+			WP_CLI::log( 'Site name field is empty' );
+			return;
+		} else {
+			if ( false === $this->_connect_to_db( 'localhost', 'root', 'AqRcKrLo' ) ) {
+				return;
+			}
+
+			$site_name = $_[0];
+			$query     = "SELECT site_name FROM ee.ee_site_data WHERE site_name LIKE '" . $site_name . "'";
+
+			if ( $result = $this->conn->query( $query ) ) {
+				if ( 0 === $result->num_rows ) {
+					WP_CLI::error(  $site_name . ' not found in database!' );
+				} else {
+					if ( ! WP_CLI\Utils\get_flag_value( $assoc_args, 'noprompt' ) ) {
+						WP_CLI::confirm( 'Are you sure you want to delete ' . $site_name . '?' );
+					}
+					$query = "DELETE FROM ee.ee_site_data WHERE site_name LIKE '" . $site_name . "'";
+					$this->conn->query( $query );
+
+					$path = getcwd();
+					$path .= '/sites/' . $site_name;
+					var_dump($path);
+					if ( ! file_exists( $path ) ) {
+						WP_CLI::error( 'Configuration file not found for: ' . $site_name );
+					}
+
+					unlink( $path );
+
+					WP_CLI::success( $site_name . ' deleted successfully' );
+				}
+			}
 		}
+
+		$this->conn->close();
 	}
 
 	public function _create_directory( $path_link = '' ) {
@@ -239,9 +275,23 @@ class EE_Command extends WP_CLI_Command {
 		// Open the file in write mode.
 		$file = fopen( $path . $file_name , 'w+' );
 
-		// Write to file.
-		foreach ( $this->printer_array as $value ) {
-			fwrite( $file, $value . PHP_EOL );
+		if ( false === $this->_connect_to_db( 'localhost', 'root', 'AqRcKrLo' ) ) {
+			return;
+		}
+
+		$query = "
+					SELECT site_type, cache_type, php_version, letsencrypt, mysql
+					FROM ee.ee_site_data
+					WHERE site_name
+					LIKE '" . $this->site_name . "'
+				";
+
+		if ( $result = $this->conn->query( $query ) ) {
+			foreach ( $result->fetch_assoc() as $key => $value ) {
+				if ( ! empty( $value ) ) {
+					fwrite( $file, $key . '=' . $value . PHP_EOL );		
+				}
+			}
 		}
 
 		fclose( $file );
