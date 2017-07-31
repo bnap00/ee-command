49cce1d5e4b79befd0b9a2d09d3b798a2c40c33f
Bharat Parsiya <bnap00@gmail.com>
2017-07-31 16:14:28 +0530

    fix path and made it non interactive

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 docker-compose up -d
 docker-compose exec wordpress apt update
-docker-compose exec wordpress apt install less git subversion
+docker-compose exec wordpress apt install -y less git subversion
 docker-compose exec wordpress chsh www-data -s /bin/bash
 docker-compose exec wordpress chown www-data:www-data . -R
 docker-compose exec wordpress curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 docker-compose exec wordpress chmod +x wp-cli.phar
 docker-compose exec wordpress mv wp-cli.phar /usr/local/bin/wp
-docker-compose exec --user www-data wordpress php -d memory_limit=256M wp package install git@github.com:Sidsector9/ee-command.git
+docker-compose exec --user www-data wordpress php -d memory_limit=256M /usr/local/bin/wp package install git@github.com:Sidsector9/ee-command.git
 alias eewp='docker-compose exec --user www-data wordpress wp'
